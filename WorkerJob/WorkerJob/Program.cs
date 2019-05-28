11a8a78432a974c806ef76b100ab3920974003cf
Marcelo Silva <marcelos@microsoft.com>
2019-05-28 17:21:56 -0500

    Create the host

--- WorkerJob/WorkerJob/Program.cs
+++ WorkerJob/WorkerJob/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Hosting;
+
 namespace WorkerJob
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices();
+                });
+
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
         }
     }
 }
